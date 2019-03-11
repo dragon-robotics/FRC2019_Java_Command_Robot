7be66388557f579bfa86d9de54a982cdda12d0c0
jc5833 <juancisneros358@gmail.com>
2019-03-10 18:45:59 -0700

    Set groundwork for command groups

---
+++ src/main/java/frc/robot/commands/Deploy_Cargo.java
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class Deploy_Cargo extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public Deploy_Cargo() {
+   //ROCKET
+    /*addSequential(new)
+    move elevator up 3 secs
+    shoot cargo 
+    move elevator down 3 secs*/
+  }
+}
